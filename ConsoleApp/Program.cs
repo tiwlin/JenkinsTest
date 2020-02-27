33ed68c8d43318020bf6dd841968f11dbd400d7d
tiw480876 <tiw480876@sina.com>
2020-02-27 13:18:17 +0800

    修改密碼

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Z.Dapper.Plus;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DemoConnectionString"].ConnectionString))
+            {
+                var order = conn.Query("select * from SalesOrder").ToList();
+                Console.WriteLine(order.Count);
+
+                var first = conn.QueryFirst("select * from SalesOrder");
+
+                using (var mulit = conn.QueryMultiple("select * from SalesOrder; select * from Customer; "))
+                {
+                    
+                }
+            }
+
+            Console.Read();
+           
+        }
+    }
+}
--- SinglePageWeb/Startup.cs
+++ SinglePageWeb/Startup.cs
@@ -13,6 +13,7 @@ namespace SinglePageWeb
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            // for test
         }
     }
 }
